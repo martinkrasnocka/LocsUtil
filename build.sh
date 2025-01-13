e15c8f8a46d5215f6078b28a0b7a9ae74c1a716b
Martin Krasnocka <martin.krasnocka@masternaut.com>
2025-01-13 10:58:23 +0100

    Updated build.sh script to generate executable file from the xcodebuild's archiving result

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
+#!/bin/zsh
 xcodebuild \
   -project LocsUtil.xcodeproj \
   -scheme LocsUtil \
   -configuration Release \
   -sdk macosx \
-  || exit 1
+  archive -archivePath $TMPDIR/LocsUtil.xcarchive \
+
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
+
+mkdir bin
+cp $TMPDIR/LocsUtil.xcarchive/Products/usr/local/bin/locsutil bin/locsutil
+rm -rf $TMPDIR/LocsUtil.xcarchive
